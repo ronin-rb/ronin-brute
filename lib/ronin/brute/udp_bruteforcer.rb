19b8b50de57dd041fd17e651799031b27be91f98
Postmodern <postmodern.mod3@gmail.com>
2023-12-22 03:49:06 -0800

    Removed unused `udp_*` methods.
    * Removed `host` and `port` arguments from `udp_connect`.

--- lib/ronin/brute/udp_bruteforcer.rb
+++ lib/ronin/brute/udp_bruteforcer.rb
@@ -32,54 +32,7 @@ module Ronin
       include Support::Network::UDP::Mixin
 
       #
-      # Tests whether a remote UDP port is open.
-      #
-      # @param [String] host
-      #   The host to connect to.
-      #
-      # @param [Integer] port
-      #   The port to connect to.
-      #
-      # @param [Hash{Symbol => Object}] kwargs
-      #   Additional keyword arguments for {#udp_connect}.
-      #
-      # @option kwargs [String, nil] :bind_host
-      #   The local host to bind to.
-      #
-      # @option kwargs [Integer, nil] :bind_port
-      #   The local port to bind to.
-      #
-      # @option kwargs [Integer] :timeout (5)
-      #   The maximum time to attempt connecting.
-      #
-      # @return [Boolean, nil]
-      #   Specifies whether the remote UDP port is open.
-      #   If no data or ICMP error were received, `nil` will be returned.
-      #
-      # @example
-      #   udp_open?
-      #   # => true
-      #
-      # @example Using a timeout:
-      #   udp_open?(timeout: 5)
-      #   # => nil
-      #
-      def udp_open?(host=params[:host],port=params[:port],**kwargs)
-        if debug?
-          print_debug "Testing if #{host}:#{port} is open ..."
-        end
-
-        super(host,port,**kwargs)
-      end
-
-      #
       # Creates a new UDPSocket object connected to a given host and port.
-      #
-      # @param [String] host
-      #   The host to connect to.
-      #
-      # @param [Integer] port
-      #   The port to connect to.
       #
       # @param [Hash{Symbol => Object}] kwargs
       #   Additional keyword arguments for {#udp_connect}.
@@ -112,125 +65,12 @@ module Ronin
       #
       # @see https://rubydoc.info/stdlib/socket/UDPSocket
       #
-      def udp_connect(host=params[:host],port=params[:port],**kwargs,&block)
+      def udp_connect(**kwargs,&block)
         if debug?
           print_debug "Connecting to #{host}:#{port} ..."
         end
 
         super(host,port,**kwargs,&block)
-      end
-
-      #
-      # Creates a new UDPSocket object, connected to a given host and port.
-      # The given data will then be written to the newly created UDPSocket.
-      #
-      # @param [String] data
-      #   The data to send through the connection.
-      #
-      # @param [String] host
-      #   The host to connect to.
-      #
-      # @param [Integer] port
-      #   The port to connect to.
-      #
-      # @param [Hash{Symbol => Object}] kwargs
-      #   Additional keyword arguments for {#udp_connect_and_send}.
-      #
-      # @option kwargs [String, nil] :bind_host
-      #   The local host to bind to.
-      #
-      # @option kwargs [Integer, nil] :bind_port
-      #   The local port to bind to.
-      #
-      # @yield [socket]
-      #   If a block is given, it will be passed the newly created socket.
-      #
-      # @yieldparam [UDPsocket] socket
-      #   The newly created UDPSocket object.
-      #
-      # @return [UDPSocket]
-      #   The newly created UDPSocket object.
-      #
-      def udp_connect_and_send(data,host=params[:host],port=params[:port],**kwargs,&block)
-        if debug?
-          print_debug "Connecting to #{host}:#{port} and sending #{data.inspect} ..."
-        end
-
-        super(data,host,port,**kwargs,&block)
-      end
-
-      #
-      # Reads the banner from the service running on the given host and
-      # port.
-      #
-      # @param [String] host
-      #   The host to connect to.
-      #
-      # @param [Integer] port
-      #   The port to connect to.
-      #
-      # @param [Hash{Symbol => Object}] kwargs
-      #   Additional keyword arguments for {#udp_banner}.
-      #
-      # @option kwargs [String, nil] :bind_host
-      #   The local host to bind to.
-      #
-      # @option kwargs [Integer, nil] :bind_port
-      #   The local port to bind to.
-      #
-      # @yield [banner]
-      #   If a block is given, it will be passed the grabbed banner.
-      #
-      # @yieldparam [String] banner
-      #   The grabbed banner.
-      #
-      # @return [String]
-      #   The grabbed banner.
-      #
-      def udp_banner(host=params[:host],port=params[:port],**kwargs)
-        if debug?
-          print_debug "Fetching the banner for #{host}:#{port} ..."
-        end
-
-        super(host,port,**kwargs)
-      end
-
-      #
-      # Connects to a specified host and port, sends the given data and then
-      # closes the connection.
-      #
-      # @param [String] data
-      #   The data to send through the connection.
-      #
-      # @param [String] host
-      #   The host to connect to.
-      #
-      # @param [Integer] port
-      #   The port to connect to.
-      #
-      # @param [Hash{Symbol => Object}] kwargs
-      #   Additional keyword arguments for {#udp_send}.
-      #
-      # @option kwargs [String, nil] :bind_host
-      #   The local host to bind to.
-      #
-      # @option kwargs [Integer, nil] :bind_port
-      #   The local port to bind to.
-      #
-      # @return [true]
-      #   The data was successfully sent.
-      #
-      # @example
-      #   buffer = "GET /" + ('A' * 4096) + "\n\r"
-      #   udp_send(buffer)
-      #   # => true
-      #
-      def udp_send(data,host=params[:host],port=params[:port],**kwargs)
-        if debug?
-          print_debug "Sending #{data.inspect} to #{host}:#{port} ..."
-        end
-
-        super(data,host,port,**kwargs)
       end
 
       #
