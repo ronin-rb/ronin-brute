0751a786fea57e0a25df51efe6e7d478761d56ba
Postmodern <postmodern.mod3@gmail.com>
2023-12-22 03:25:12 -0800

    Added a naive POP3 bruteforcer.
    * Need to add a timeout to disconnect/re-connect on invalid login
      timeouts.

---
+++ lib/ronin/brute/builtin/pop3.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+#
+# ronin-brute - A micro-framework and tool for bruteforcing credentials.
+#
+# Copyright (c) 2023 Hal Brodigan (postmodern.mod3@gmail.com)
+#
+# ronin-brute is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-brute is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-brute.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/brute/tcp_bruteforcer'
+
+module Ronin
+  module Brute
+    #
+    # A POP3 login bruteforcer.
+    #
+    class POP3 < TCPBruteforcer
+
+      register 'pop3'
+
+      port 110
+      ssl_port 995
+
+      param :domain, desc: 'The mailserver domain name'
+
+      #
+      # The email server domain name.
+      #
+      # @return [String]
+      #   The `domain` param or the `host` param.
+      #
+      def domain
+        params[:domain] || params[:host]
+      end
+
+      #
+      # Bruteforces a FTP server.
+      #
+      # @param [Async::LimitedQueue] credentials
+      #   The credentials to test.
+      #
+      # @yield [username, password]
+      #   The given block will be passed each valid username and password
+      #   combination from the credentials list.
+      #
+      # @yieldparam [String] username
+      #   A valid username.
+      #
+      # @yieldparam [String] password
+      #   A valid password.
+      #
+      def bruteforce(credentials)
+        tcp_connect do |socket|
+          # ignore the banner
+          socket.gets
+
+          while (username, password = credentials.dequeue)
+            socket.puts "USER #{username}@#{domain}"
+
+            if socket.gets.chomp == "+OK"
+              socket.puts "PASS #{password}"
+
+              if socket.gets.chomp == "+OK Logged in."
+                yield username, password
+              end
+            end
+          end
+        end
+      end
+
+    end
+  end
+end
