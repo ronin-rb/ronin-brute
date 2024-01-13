a9d6968ec51dcdea8a74b9c022bc7b57e9372537
Postmodern <postmodern.mod3@gmail.com>
2024-01-13 02:51:59 -0800

    Updated the copyright years.

--- lib/ronin/brute/builtin/pop3.rb
+++ lib/ronin/brute/builtin/pop3.rb
@@ -2,7 +2,7 @@
 #
 # ronin-brute - A micro-framework and tool for bruteforcing credentials.
 #
-# Copyright (c) 2023 Hal Brodigan (postmodern.mod3@gmail.com)
+# Copyright (c) 2023-2024 Hal Brodigan (postmodern.mod3@gmail.com)
 #
 # ronin-brute is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
