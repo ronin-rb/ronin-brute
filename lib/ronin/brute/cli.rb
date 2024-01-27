1dc20351bf4b4bda1668b4f5f0f645d444902344
Postmodern <postmodern.mod3@gmail.com>
2024-01-27 04:43:11 -0800

    Added the `ronin-brute` command (closes #3).

---
+++ lib/ronin/brute/cli.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+#
+# ronin-brute - A micro-framework and tool for bruteforcing credentials.
+#
+# Copyright (c) 2023-2024 Hal Brodigan (postmodern.mod3@gmail.com)
+#
+# ronin-brute is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-brute is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-brute.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/brute/version'
+
+require 'command_kit/commands'
+require 'command_kit/commands/auto_load'
+require 'command_kit/options/version'
+
+module Ronin
+  module Brute
+    #
+    # The `ronin-brute` command-line interface (CLI).
+    #
+    # @api private
+    #
+    class CLI
+
+      include CommandKit::Commands
+      include CommandKit::Commands::AutoLoad.new(
+        dir:       "#{__dir__}/cli/commands",
+        namespace: "#{self}::Commands"
+      )
+      include CommandKit::Options::Version
+
+      command_name 'ronin-brute'
+      version Ronin::Brute::VERSION
+
+    end
+  end
+end
