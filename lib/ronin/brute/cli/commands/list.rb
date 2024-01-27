4c72a307f558f3b0e4fa0db4ec47a1305cdfe4c7
Postmodern <postmodern.mod3@gmail.com>
2024-01-27 05:00:29 -0800

    Added the `ronin-brute list` command (closes #5).

---
+++ lib/ronin/brute/cli/commands/list.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+#
+# ronin-brute - A micro-framework and tool for bruteforcing credentials.
+#
+# Copyright (c) 2023-2024 Hal Brodigan (postmodern.mod3@gmail.com)
+#
+# ronin-brute is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-brute is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-brute.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/brute/registry'
+require 'ronin/brute/cli/command'
+
+module Ronin
+  module Brute
+    class CLI
+      module Commands
+        #
+        # Lists all available bruteforcers.
+        #
+        # ## Usage
+        #
+        #     ronin-brute list [options] [DIR]
+        #
+        # ## Options
+        #
+        #     -h, --help                       Print help information
+        #
+        # ## Arguments
+        #
+        #     [DIR]                            The optional bruteforcer directory to list
+        #
+        class List < Command
+
+          usage '[options] [DIR]'
+
+          argument :dir, required: false,
+                         desc:     'The optional bruteforcer directory to list'
+
+          description 'Lists the available bruteforcers'
+
+          man_page 'ronin-brute-list.1'
+
+          #
+          # Runs the `ronin-brute list` command.
+          #
+          # @param [String, nil] dir
+          #   The optional bruteforcer directory to list.
+          #
+          def run(dir=nil)
+            files = if dir
+                      dir = "#{dir}/" unless dir.end_with?('/')
+
+                      Brute.list_files.select do |file|
+                        file.start_with?(dir)
+                      end
+                    else
+                      Brute.list_files
+                    end
+
+            files.each do |file|
+              puts "  #{file}"
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ man/ronin-brute-list.1.md
@@ -0,0 +1,31 @@
+# ronin-brute-list 1 "2024-01-01" Ronin Brute "User Manuals"
+
+## NAME
+
+ronin-brute-list - Lists all available bruteforcers
+
+## SYNOPSIS
+
+`ronin-brute list` [*options*] [*DIR*]
+
+## DESCRIPTION
+
+Lists all available bruteforcers.
+
+## ARGUMENTS
+
+*DIR*
+: The optional bruteforcer directory to filter by.
+
+## OPTIONS
+
+`-h`, `--help`
+: Prints help information.
+
+## AUTHOR
+
+Postmodern <postmodern.mod3@gmail.com>
+
+## SEE ALSO
+
+[ronin-brute](ronin-brute.1.md) [ronin-brute-show](ronin-brute-show.1.md)
