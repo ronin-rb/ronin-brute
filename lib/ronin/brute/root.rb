37fe11849525a1baeb54b28729a59e2cd92accdf
Postmodern <postmodern.mod3@gmail.com>
2024-01-27 04:49:54 -0800

    Added the `Ronin::Brute::ROOT` constant.

---
+++ lib/ronin/brute/root.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+#
+# ronin-brute - A micro-framework and tool for bruteforcing credentials.
+#
+# Copyright (c) 2023-2024 Hal Brodigan (postmodern.mod3@gmail.com)
+#
+# ronin-brute is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-brute is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-brute.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+module Ronin
+  module Brute
+    # Path to `ronin-brute` root directory.
+    #
+    # @api private
+    ROOT = File.expand_path(File.join(__dir__,'..','..','..'))
+  end
+end
