fdaa3a07ad5cf4cd95013a9cbf687c9c3ea36c2e
Postmodern <postmodern.mod3@gmail.com>
2024-01-27 06:10:47 -0800

    Added missing license header.

--- lib/ronin/brute/cli/commands/completion.rb
+++ lib/ronin/brute/cli/commands/completion.rb
@@ -1,3 +1,23 @@
+# frozen_string_literal: true
+#
+# ronin-brute - A micro-framework and tool for bruteforcing credentials.
+#
+# Copyright (c) 2023-2024 Hal Brodigan (postmodern.mod3@gmail.com)
+#
+# ronin-brute is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-brute is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with ronin-brute.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 require 'ronin/brute/root'
 require 'ronin/core/cli/completion_command'
 
